935c35aa49aebe1e4ba2fb042858d35ec179f8c8
Ulul Fadhli <ulfdh244@gmail.com>
2020-01-06 17:55:44 +0700

    revisi exercises 6

--- Exercises-6.js
+++ Exercises-6.js
@@ -3,18 +3,20 @@ function angkaPalindrome(num) {
     while (true) {
         num++
         var strNum = String(num)
-        var ubahNum = Number(strNum)
-        if (num === ubahNum) {
+        var string = ''
+        for (let i = strNum.length - 1; i >= 0; i--) {
+            string += strNum[i]
+        }
+
+        if (num == string) {
             return num
         }
     }
+}
 
-    
-  }
-  
-  // TEST CASES
-  console.log(angkaPalindrome(8)); // 9
-  console.log(angkaPalindrome(10)); // 11
-  console.log(angkaPalindrome(117)); // 121
-  console.log(angkaPalindrome(175)); // 181
-  console.log(angkaPalindrome(1000)); // 1001
+// TEST CASES
+console.log(angkaPalindrome(8)); // 9
+console.log(angkaPalindrome(10)); // 11   //118   undefined811
+console.log(angkaPalindrome(117)); // 121
+console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(1000)); // 1001
